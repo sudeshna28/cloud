396ce2fd96ec2c9c583977ea6e77a0857221a60c
Basu <836986@cognizant.com>
2020-01-20 18:14:14 +0530

    welcome

--- CI.java
+++ CI.java
@@ -1,5 +1,5 @@
 class CI{  
     public static void main(String args[]){  
-     System.out.println("git gui abcdef project");  
+     System.out.println("git gui abcdef 123456 project");  
     }  
 }  
